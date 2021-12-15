dc799f5674e2d4d2f1938c8a5a154223119aeba9
Andreas Karlsson <andreas@proxel.se>
2021-12-15 10:24:15 +0100

    Some more cleanup

--- 2020/23/23b.zig
+++ 2020/23/23b.zig
@@ -5,10 +5,6 @@ const parseU = std.fmt.parseUnsigned;
 const M: usize = 1000000;
 const N: usize = 10000000;
 
-const Foo = struct {
-    next: usize,
-};
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}) {};
     const allocator = &gpa.allocator;
@@ -23,43 +19,43 @@ pub fn main() !void {
 
     var cc = try reader.readUntilDelimiterAlloc(allocator, '\n', 10000);
 
-    var data = try allocator.alloc(Foo, M);
+    var data = try allocator.alloc(usize, M);
 
     for (cc) |_, i| {
         const x = std.mem.indexOfScalar(u8, cc, @intCast(u8, i) + '1').? + 1;
 
         if (x == cc.len) {
-            data[i].next = cc.len;
+            data[i] = cc.len;
         } else {
-            data[i].next = cc[x] - '1';
+            data[i] = cc[x] - '1';
         }
     }
 
     var ii: usize = cc.len;
     while (ii < M) : (ii += 1) {
-        data[ii].next = ii + 1;
+        data[ii] = ii + 1;
     }
 
     var cur: usize = cc[0] - '1';
 
-    data[M - 1].next = cur;
+    data[M - 1] = cur;
 
     var r: usize = 0;
     while (r < N) : (r += 1) {
-        const x1 = data[cur].next;
-        const x2 = data[x1].next;
-        const x3 = data[x2].next;
+        const x1 = data[cur];
+        const x2 = data[x1];
+        const x3 = data[x2];
 
         var dest = (cur + M - 1) % M;
 
         while (dest == x1 or dest == x2 or dest == x3) dest = (dest + M - 1) % M;
 
-        data[cur].next = data[x3].next;
-        data[x3].next = data[dest].next;
-        data[dest].next = x1;
+        data[cur] = data[x3];
+        data[x3] = data[dest];
+        data[dest] = x1;
 
-        cur = data[cur].next;
+        cur = data[cur];
     }
 
-    std.debug.print("{}\n", .{(data[0].next + 1) * (data[data[0].next].next + 1)});
+    std.debug.print("{}\n", .{(data[0] + 1) * (data[data[0]] + 1)});
 }
